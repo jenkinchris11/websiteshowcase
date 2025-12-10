c2a87dfaf7218a4df8d4112a87c082ca24916527
jenkinchris11 <jenkinchris11@gmail.com>
2025-12-10 17:38:29 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -250,10 +250,9 @@ onBeforeUnmount(() => {
 .lightrope {
   text-align: center;
   white-space: nowrap;
-  overflow: hidden;
   position: absolute;
   z-index: 1;
-  margin: -15px 0 0 0;
+  margin: -25px 0 0 0;
   padding: 0;
   pointer-events: none;
   width: 100%;
