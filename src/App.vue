7a8229decc1be94fd6155e091fd7b63f73ee2fdb
jenkinchris11 <jenkinchris11@gmail.com>
2025-12-10 17:51:38 +0000

    Adjust lightrope alternating color

--- src/App.vue
+++ src/App.vue
@@ -272,24 +272,24 @@ onBeforeUnmount(() => {
   border-radius: 50%;
   margin: 20px;
   display: inline-block;
-  background: #00f7a5;
-  box-shadow: 0px 4.6666666667px 24px 3px #00f7a5;
+  background: #e41311;
+  box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   -webkit-animation-name: flash-1;
           animation-name: flash-1;
   -webkit-animation-duration: 2s;
           animation-duration: 2s;
 }
 .lightrope li:nth-child(2n+1) {
-  background: aqua;
-  box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 255, 255, 0.5);
+  background: #008556;
+  box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 133, 86, 0.5);
   -webkit-animation-name: flash-2;
           animation-name: flash-2;
   -webkit-animation-duration: 0.4s;
           animation-duration: 0.4s;
 }
 .lightrope li:nth-child(4n+2) {
-  background: #f70094;
-  box-shadow: 0px 4.6666666667px 24px 3px #f70094;
+  background: #e41311;
+  box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   -webkit-animation-name: flash-3;
           animation-name: flash-3;
   -webkit-animation-duration: 1.1s;
@@ -332,63 +332,63 @@ onBeforeUnmount(() => {
 
 @-webkit-keyframes flash-1 {
   0%, 100% {
-    background: #00f7a5;
-    box-shadow: 0px 4.6666666667px 24px 3px #00f7a5;
+    background: #e41311;
+    box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   }
   50% {
-    background: rgba(0, 247, 165, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 247, 165, 0.2);
+    background: rgba(228, 19, 17, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(228, 19, 17, 0.2);
   }
 }
 
 @keyframes flash-1 {
   0%, 100% {
-    background: #00f7a5;
-    box-shadow: 0px 4.6666666667px 24px 3px #00f7a5;
+    background: #e41311;
+    box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   }
   50% {
-    background: rgba(0, 247, 165, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 247, 165, 0.2);
+    background: rgba(228, 19, 17, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(228, 19, 17, 0.2);
   }
 }
 @-webkit-keyframes flash-2 {
   0%, 100% {
-    background: aqua;
-    box-shadow: 0px 4.6666666667px 24px 3px aqua;
+    background: #008556;
+    box-shadow: 0px 4.6666666667px 24px 3px #008556;
   }
   50% {
-    background: rgba(0, 255, 255, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 255, 255, 0.2);
+    background: rgba(0, 133, 86, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 133, 86, 0.2);
   }
 }
 @keyframes flash-2 {
   0%, 100% {
-    background: aqua;
-    box-shadow: 0px 4.6666666667px 24px 3px aqua;
+    background: #008556;
+    box-shadow: 0px 4.6666666667px 24px 3px #008556;
   }
   50% {
-    background: rgba(0, 255, 255, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 255, 255, 0.2);
+    background: rgba(0, 133, 86, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(0, 133, 86, 0.2);
   }
 }
 @-webkit-keyframes flash-3 {
   0%, 100% {
-    background: #f70094;
-    box-shadow: 0px 4.6666666667px 24px 3px #f70094;
+    background: #e41311;
+    box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   }
   50% {
-    background: rgba(247, 0, 148, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(247, 0, 148, 0.2);
+    background: rgba(228, 19, 17, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(228, 19, 17, 0.2);
   }
 }
 @keyframes flash-3 {
   0%, 100% {
-    background: #f70094;
-    box-shadow: 0px 4.6666666667px 24px 3px #f70094;
+    background: #e41311;
+    box-shadow: 0px 4.6666666667px 24px 3px #e41311;
   }
   50% {
-    background: rgba(247, 0, 148, 0.4);
-    box-shadow: 0px 4.6666666667px 24px 3px rgba(247, 0, 148, 0.2);
+    background: rgba(228, 19, 17, 0.4);
+    box-shadow: 0px 4.6666666667px 24px 3px rgba(228, 19, 17, 0.2);
   }
 }
   
