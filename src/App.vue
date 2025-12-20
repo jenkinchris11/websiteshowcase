ab6fa62d292f75dfeab5f8c72a11bf1c1ce0efcc
jenkinchris11 <jenkinchris11@gmail.com>
2025-12-20 18:13:54 +0000

    Revert "Import DotLottie component in App"

--- src/App.vue
+++ src/App.vue
@@ -615,8 +615,8 @@
 </template>
 
 <script setup>
-import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 import emailjs from '@emailjs/browser'
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 import ProjectCard from './components/ProjectCard.vue'
 import HeroCanvas from './components/HeroCanvas.vue'
 import ChatWidget from './components/ChatWidget.vue'
